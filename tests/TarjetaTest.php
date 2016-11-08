14140c457815a9cce52903188739ee2ddc725812
YacoYacomini <fyacomini@hotmail.com>
2016-11-08 11:33:34 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -90,39 +90,39 @@ class BoletoTest extends \PHPUnit_Framework_TestCase {
 	public function testPlus(){
 		$this->tarjeta->pagar($this->A,"2016/02/1 12:00");
 		$this->assertEquals($this->tarjeta->getBoleto()->getBoleto(),
-			"SEMTUR - \n2016/02/1 12:00 Linea:K\nPLUS $8\nSaldo: $0"," ");
+			"SEMTUR - \n2016/02/1 12:00 Linea:112\nPLUS $8\nSaldo: $0"," ");
 	}
 	public function testUltPlus(){
 		$this->medio->pagar($this->A,"2016/01/1 12:00");
 		$this->medio->pagar($this->A,"2016/02/1 12:00");
 		$this->assertEquals($this->medio->getBoleto()->getBoleto(),
-			"SEMTUR - \n2016/02/1 12:00 Linea:K\nULT. PLUS $4\nSaldo: $0"," ");
+			"SEMTUR - \n2016/02/1 12:00 Linea:112\nULT. PLUS $4\nSaldo: $0"," ");
 	}
 	public function testMedio(){
 		$this->medio->recargar(20);
 		$this->medio->pagar($this->A,"2016/02/1 12:00");
 		$this->assertEquals($this->medio->getBoleto()->getBoleto(),
-			"SEMTUR - \n2016/02/1 12:00 Linea:K\nMEDIO $4\nSaldo: $16"," ");
+			"SEMTUR - \n2016/02/1 12:00 Linea:112\nMEDIO $4\nSaldo: $16"," ");
 	}
 	public function testTransbordo(){
 		$this->tarjeta->recargar(30);
 		$this->tarjeta->pagar($this->B,"2016/02/1 11:55");
 		$this->tarjeta->pagar($this->A,"2016/02/1 12:00");
 		$this->assertEquals($this->tarjeta->getBoleto()->getBoleto(),
-			"SEMTUR - \n2016/02/1 12:00 Linea:K\nTRANSBORDO $2.64\nSaldo: $19.36"," ");
+			"SEMTUR - \n2016/02/1 12:00 Linea:112\nTRANSBORDO $2.64\nSaldo: $19.36"," ");
 	}
 	public function testTransbordoMedio(){
 		$this->medio->recargar(30);
 		$this->medio->pagar($this->B,"2016/02/1 11:55");
 		$this->medio->pagar($this->A,"2016/02/1 12:00");
 		$this->assertEquals($this->medio->getBoleto()->getBoleto(),
-			"SEMTUR - \n2016/02/1 12:00 Linea:K\nTRANSBORDO $1.32\nSaldo: $24.68"," ");
+			"SEMTUR - \n2016/02/1 12:00 Linea:112\nTRANSBORDO $1.32\nSaldo: $24.68"," ");
 	}
 	public function testNormal(){
 		$this->tarjeta->recargar(8);
 		$this->tarjeta->pagar($this->A,"2016/02/1 12:00");
 		$this->assertEquals($this->tarjeta->getBoleto()->getBoleto(),
-			"SEMTUR - \n2016/02/1 12:00 Linea:K\nNORMAL $8\nSaldo: $0"," ");
+			"SEMTUR - \n2016/02/1 12:00 Linea:112\nNORMAL $8\nSaldo: $0"," ");
 	}
 }
 ?>
